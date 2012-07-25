4e6d06ffb552c5b67abc5a9e6eb38f7f9679d165
Justin Barton <bartonjustin@gmail.com>
2012-07-25 23:26:18 +1200

    tidyup

--- src/evstream/core.clj
+++ src/evstream/core.clj
@@ -9,37 +9,35 @@
   ([term] (take-while (complement term) (event-stream)))
   ([t v] (map #(deref % t v) (seque @promises)))
   ([t v term] (take-while (complement term) (event-stream t v)))
-  ;([terminator] (take-while terminator (event-stream)))
-  ;([timeout-ms timeout-val term] (event-stream #(term (deref % timeout-ms timeout-val))))
   )
 
-;; consumers
 
-;; with a timeout specified the consumer can get ahead of producer -
-;; but thats as expected - nth is allowed too.
+(comment
+  ;; producers
+  ;; could hook up producers to zeromq for inter process event-processing.
+  
+  ;; consumers
+  ;; can be written using standard seq lib
 
-(future (do (doseq [e (take 10 (event-stream 10000 "x"))]
-              (println "stream1: " e))
-            (println "stream1 done.")))
+  ;; with a timeout specified the consumer can get ahead of producer -
+  ;; but thats as expected - nth is allowed too.
 
-(future (do (doseq [e (take 10 (event-stream 10000 "x" #(= % "bla")))]
-              (println "stream2: " e))
-            (println "stream2 done.")))
+  ;; overall timeout could be via future-cancel
+  
+(future (doseq [e (take 10 (event-stream 10000 "x"))] (println "stream1: " e))
+        (println "stream1 done."))
 
-(future (doseq [e (filter #(.startsWith % "bla") (event-stream))]
-          (print "stream3:" e)))
+(future (doseq [e (take 10 (event-stream 10000 "x" #(= % "bla")))] (println "stream2: " e))
+        (println "stream2 done."))
+
+(future (doseq [e (filter #(.startsWith % "bla") (event-stream))] (print "stream3:" e)))
 
 (future (let [x (nth (event-stream) 3)] (print "future nth (4th event):" x)))
 
 (future (doseq [x (event-stream #(= % "bla"))] (print "future term:" x)) (println "future term done."))
 
-;; overall timeout could be via future-cancel
+)
 
 
 
 
-;; producers
-;(map #(deliver %1 (str "bla" %2)) evpromises (range 100))
-;(deliver (first (filter (complement realized?) evpromises)) "blabbb")
-
-;(defn event-stream [timeout-ms timeout-val terminator] (map #(deref % timeout-ms timeout-val) (take-while (comp terminator) (seque @promises))))
