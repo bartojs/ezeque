a85f5e25c68f8b09bf4cb52542b9da1e2cc45357
Justin Barton <bartonjustin@gmail.com>
2012-07-25 21:38:50 +1200

    promises as an atom and raise! swaps the seq

--- src/evstream/core.clj
+++ src/evstream/core.clj
@@ -1,15 +1,21 @@
 (ns evstream.core)
 
-(def evpromises (repeatedly promise))
-(defn evstream [] (seque evpromises))
+(def promises (atom (repeatedly promise)))
+(defn raise! [event]
+  (swap! promises (fn [p] (deliver (first p) event) (drop 1 p))) event)
+(defn event-stream [] (seque @promises))
 
 ;; consumers
-(future (do (doseq [e (take 10 (evstream))] (println "stream1: " @e)) (println "stream1 done.")))
+(future (do (doseq [e (take 10 (evstream))]
+              (println "stream1: " @e))
+            (println "stream1 done.")))
 
 (future (doseq [e (filter #(.startsWith (deref %) "bla") (evstream))]
           (print "stream2:" @e)))
 
+
 ;; producers
-(take 3 (map #(deliver % "moo") evpromises))               
-(map #(deliver %1 (str "bla" %2)) evpromises (range 100))
-(deliver (first (filter (complement realized?) evpromises)) "blabbb")
+;(map #(deliver %1 (str "bla" %2)) evpromises (range 100))
+;(deliver (first (filter (complement realized?) evpromises)) "blabbb")
+
+;(defn event-stream [timeout-ms timeout-val terminator] (map #(deref % timeout-ms timeout-val) (take-while (comp terminator) (seque @promises))))
