34299354b04fe20bc3d6b498a94e1724bcbb9262
Justin Barton <bartonjustin@gmail.com>
2012-08-02 21:41:41 +1200

    split out project ready for github

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ezeque "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://github.com/bartonjustin/ezeque"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :jvm-opts ["-Xdebug" "-Xrunjdwp:transport=dt_socket,server=y,suspend=n"])
